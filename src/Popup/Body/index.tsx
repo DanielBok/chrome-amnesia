5c9b57f2ebd867ff11d10cf52ff45c25eaa9d803
Daniel Bok <daniel.bok@outlook.com>
2019-12-28 12:44:39 +0800

    Add styles to body

--- src/Popup/Body/index.tsx
+++ src/Popup/Body/index.tsx
@@ -3,12 +3,13 @@ import React from "react";
 import { useSelector } from "react-redux";
 import RuleBox from "./RuleBox";
 import RuleBoxTitle from "./RuleBox/RuleBoxTitle";
+import styles from "./styles.less";
 
 export default () => {
   const rules = useSelector(SyncSelectors.rulesArray);
 
   return (
-    <div>
+    <div className={styles.body}>
       <RuleBoxTitle />
       {rules.map(rule => (
         <RuleBox key={rule} rule={rule} />
--- src/Popup/index.tsx
+++ src/Popup/index.tsx
@@ -1,14 +1,14 @@
 import { Layout } from "antd";
 import React from "react";
+
 import Body from "./Body";
-
 import Header from "./Header";
+import Footer from "./Footer";
 
-export default () => {
-  return (
-    <Layout>
-      <Header />
-      <Body />
-    </Layout>
-  );
-};
+export default () => (
+  <Layout>
+    <Header />
+    <Body />
+    <Footer />
+  </Layout>
+);
