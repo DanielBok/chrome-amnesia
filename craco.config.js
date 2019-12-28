5d889ed1d797f35a1555871db4bc1970be92670b
Daniel Bok <daniel.bok@outlook.com>
2019-12-28 10:12:14 +0800

    Added additional configs to Craco for "less" support

--- craco.config.js
+++ craco.config.js
@@ -1,3 +1,6 @@
+const CracoAntDesignPlugin = require("craco-antd");
+const WebpackBar = require("webpackbar");
+
 const path = require("path");
 
 module.exports = {
@@ -7,6 +10,24 @@ module.exports = {
     },
     alias: {
       "@": path.resolve(__dirname, "src/")
+    },
+    plugins: [new WebpackBar({ profile: true })]
+  },
+  plugins: [
+    {
+      plugin: CracoAntDesignPlugin,
+      options: {
+        lessLoaderOptions: {
+          javascriptEnabled: true,
+          noIeCompat: true
+        },
+        cssLoaderOptions: {
+          modules: {
+            localIdentName: "[name]__[local]__[hash:base64:5]"
+          },
+          localsConvention: "camelCase"
+        }
+      }
     }
-  }
+  ]
 };
