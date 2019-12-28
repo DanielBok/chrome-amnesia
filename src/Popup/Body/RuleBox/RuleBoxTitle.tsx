320d3b06797d43cb0c40ea6245b5fb74c95b3274
Daniel Bok <daniel.bok@outlook.com>
2019-12-28 12:11:16 +0800

    Add info box

--- src/Popup/Body/RuleBox/RuleBoxTitle.tsx
+++ src/Popup/Body/RuleBox/RuleBoxTitle.tsx
@@ -1,14 +1,38 @@
-import styles from "@/Popup/Body/RuleBox/styles.less";
-import { Col, Row } from "antd";
+import { Col, Icon, Row, Tooltip } from "antd";
 import React from "react";
+import styles from "./styles.less";
 
 export default () => (
   <div>
     <Row gutter={8} className={styles.container}>
       <Col span={22}>
         <code>Exclusion Rule</code>
+        <InfoBox />
       </Col>
       <Col span={2} />
     </Row>
   </div>
 );
+
+const InfoBox = () => {
+  const title = (
+    <>
+      Rules are regular expressions. If in doubt, use
+      <br />
+      <code style={{ background: "grey", whiteSpace: "nowrap" }}>
+        yourUrl.match(new Regexp(String.raw`yourRule`))
+      </code>
+      <br />
+      to test and validate.
+    </>
+  );
+  return (
+    <Tooltip
+      placement="bottom"
+      title={title}
+      overlayClassName={styles.infoTooltip}
+    >
+      <Icon type="info-circle" className={styles.infoIcon} />
+    </Tooltip>
+  );
+};
